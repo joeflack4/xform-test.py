43474fc2f614eba8ceb9ce2d52d022dedf84aac7
joeflack4 <joeflack4@gmail.com>
2018-10-18 12:58:29 -0400

    - Added test/ dir scaffolding. - Removed research folder. - Removed python implementation. - Updated requirements.

--- xform_test/__main__.py
+++
@@ -1,6 +0,0 @@
-"""Main."""
-
-
-if __name__ == '__main__':
-    from xform_test.interfaces.cli import cli
-    cli()
--- xform_test/config.py
+++
@@ -1,6 +0,0 @@
-"""Config."""
-from xform_test.definitions.abstractions import pwd
-
-
-PKG_DIR = pwd()
-JAVAROSA_BINARY_PATH = PKG_DIR + '/bin/opendatakit-javarosa-2.11.0.jar'
